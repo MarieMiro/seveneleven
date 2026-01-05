7ab88e83aa489ad30175b557a362a9c86b66cd70
maria <mariayushko61@gmail.com>
2026-01-05 13:37:51 +0700

    Prepare project for render deploy

--- bboard/bboard/settings.py
+++ bboard/bboard/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -158,3 +158,8 @@ THUMBNAIL_BASEDIR = 'thumbnails'
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_URLS_REGEX = r'^/api/.*$'
+
+ALLOWED_HOSTS = ['*']
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+DEBUG = False
